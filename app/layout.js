aedf49f6b85f7c2c64bb954df8cf248f0d45ee36
rudraupasani <rudraupasani2007@gmail.com>
2026-02-05 18:26:37 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
